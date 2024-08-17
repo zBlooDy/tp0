f77ded53393b61ed32c0a034acb7bd59f43e458d
Thiago <tperezgiacchetta@frba.utn.edu.ar>
2024-08-17 17:45:06 +0000

    TP0 terminado

--- client/src/utils.c
+++ client/src/utils.c
@@ -26,14 +26,18 @@ int crear_conexion(char *ip, char* puerto)
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(ip, puerto, &hints, &server_info);
+	int err;
+
+	err = getaddrinfo(ip, puerto, &hints, &server_info);
 
 	// Ahora vamos a crear el socket.
-	int socket_cliente = 0;
+	int socket_cliente = socket(server_info->ai_family,
+                         server_info->ai_socktype,
+                         server_info->ai_protocol);
 
 	// Ahora que tenemos el socket, vamos a conectarlo
-
-
+	err = connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
+	
 	freeaddrinfo(server_info);
 
 	return socket_cliente;

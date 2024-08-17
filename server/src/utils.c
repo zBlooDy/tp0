60539f5ac00ea085f84d78440f0102eb69b7e9d3
Thiago <tperezgiacchetta@frba.utn.edu.ar>
2024-08-17 17:47:42 +0000

    TP0 terminado

--- server/src/utils.c
+++ server/src/utils.c
@@ -4,10 +4,11 @@ t_log* logger;
 
 int iniciar_servidor(void)
 {
-	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+
 
 	int socket_servidor;
+
+	int err;
 
 	struct addrinfo hints, *servinfo, *p;
 
@@ -16,13 +17,19 @@ int iniciar_servidor(void)
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
+	err = getaddrinfo(NULL, PUERTO, &hints, &servinfo);
 
+	socket_servidor = socket(servinfo->ai_family,
+                        servinfo->ai_socktype,
+                        servinfo->ai_protocol);
 	// Creamos el socket de escucha del servidor
 
+	
 	// Asociamos el socket a un puerto
-
+	err = bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
+	
 	// Escuchamos las conexiones entrantes
+	err = listen(socket_servidor, SOMAXCONN);
 
 	freeaddrinfo(servinfo);
 	log_trace(logger, "Listo para escuchar a mi cliente");
@@ -32,11 +39,12 @@ int iniciar_servidor(void)
 
 int esperar_cliente(int socket_servidor)
 {
-	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
 
 	// Aceptamos un nuevo cliente
 	int socket_cliente;
+
+	socket_cliente = accept(socket_servidor, NULL, NULL);
+
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
